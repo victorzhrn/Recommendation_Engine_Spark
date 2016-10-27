bfd21386b3980ab334a8bba7891aa86bf89eadc2
Ruinan Zhang <zhangr4@rose-hulman.edu>
2016-10-26 21:36:57 -0400

    create tables

---
+++ create_dbs.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS spark_data;
+
+use spark_data;
+
+CREATE TABLE IF NOT EXISTS pv_sample (
+uuid VARCHAR(20),
+document_id INT,
+timestamp INT,
+platform SMALLINT);
+
+CREATE TABLE IF NOT EXISTS export_pv_sample(
+uuid VARCHAR(20),
+document_id INT,
+timestamp INT,
+platform SMALLINT);
+
+CREATE TABLE IF NOT EXISTS clicks_train (
+display_id INT,
+ad_id BIGINT,
+clicked BINARY);
+
+CREATE TABLE IF NOT EXISTS clicks_test (
+display_id INT,
+ad_id BIGINT);
+
+CREATE TABLE IF NOT EXISTS events_test (
+display_id INT,
+uuid VARCHAR(20),
+document_id INT,
+timestamp INT,
+platform SMALLINT);
+
+CREATE TABLE IF NOT EXISTS promoted_content (
+ad_id BIGINT,
+document_id INT);
