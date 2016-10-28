bb090c3f6609a33ac33bd68a3a7a8481d8642934
Ruinan Zhang <zhangr4@rose-hulman.edu>
2016-10-28 11:25:23 -0400

    initialize explicit rec

---
+++ ExplicitRecommendation/src/Explitcit.java
@@ -0,0 +1,15 @@
+import scala.Tuple2;
+
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.recommendation.ALS;
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
+import org.apache.spark.mllib.recommendation.Rating;
+import org.apache.spark.SparkConf;
+
+public class Explitcit {
+	
+	public static void main(String[] args){
+		
+	}
+}
